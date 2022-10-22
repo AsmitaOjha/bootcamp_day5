6783bb359652d7bc4a17f5715d009580882d1992
AsmitaOjha <asmitaojha05@gmail.com>
2022-10-22 16:31:45 +0545

    Day 5 of bootcamp

---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("Hello, I am K.D. Ojha\n");
+    printf("I am a student of Shree Shanti Niketan Secondary School.\n");
+    return 0;
+}
